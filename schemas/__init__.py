499febfd4241a8b4c74a6a3672f91ade69d2a078
HumbertoSGoncalves <130871047+HumbertoSGoncalves@users.noreply.github.com>
2023-09-21 20:14:59 -0300

    Add files via upload

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,1 +1,5 @@
-
+from schemas.nota import NotaSchema
+from schemas.vinho import VinhoSchema, VinhoBuscaSchema, VinhoViewSchema, \
+                            ListagemVinhosSchema, VinhoUpdateSchema, VinhoDelSchema, apresenta_vinhos, \
+                            apresenta_vinho
+from schemas.error import ErrorSchema
--- schemas/nota.py
+++ schemas/nota.py
@@ -1,1 +1,7 @@
+from pydantic import BaseModel
 
+
+# como uma nota é adicionada a um vinho
+class NotaSchema(BaseModel):
+    vinho_id: int = 1
+    texto: str = "Notas a serem adicionadas."
